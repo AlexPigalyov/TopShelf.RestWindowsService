ae11c353ba0ddc8118f4d6546cfae564020590d0
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-09 22:57:15 +0300

    add StartUp

---
+++ TopShelf.RestWindowsService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace TopShelf.RestWindowsService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            app.Use(config);
+        }
+    }
+}
